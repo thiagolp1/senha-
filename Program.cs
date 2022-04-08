84cf81790d645de2d5d2709905496f04ff2eb2e4
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-08 19:39:02 -0300

    finalizaçaõ do exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿String acessoP, senha;
+const string senhacorreta  = "thi123";
+
+Console.Write("Olá, usuário. Por favor, digite sua senha:");
+senha = Console.ReadLine()!;
+
+acessoP = senha == senhacorreta ? "permitido" : "negado";
+
+Console.WriteLine($"Acesso { acessoP }");
